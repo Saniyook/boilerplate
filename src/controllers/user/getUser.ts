898bbb7f5ab7cd9bdd6c56544cff05a80c06db8f
Alexander Izhemtsev <Alexander.Izhemtsev@noveogroup.com>
2020-11-26 12:51:52 +0300

    added typed response to get user

--- src/controllers/user/getUser.ts
+++ src/controllers/user/getUser.ts
@@ -6,8 +6,8 @@ import {
   requestBody,
   getModelSchemaRef,
 } from '@loopback/openapi-v3';
-import { Entity, model, Model, property } from '@loopback/repository';
-import { SomeError, SuccessModel } from '../responseModels';
+import { model, Model, property } from '@loopback/repository';
+import { SomeError } from '../responseModels';
 
 @model()
 class Address {
@@ -27,19 +27,35 @@ class User {
   address!: Address;
 }
 
+@model()
+class UserWithNumber extends Model {
+  @property({ type: User })
+  user!: User;
+
+  @property({ type: 'array', itemType: 'number' })
+  numbers!: number[];
+
+  constructor(data: Partial<UserWithNumber>) {
+    super(data);
+  }
+}
+
 export default class GetUserController {
   @post('/login/{id}')
-  @response(200, SuccessModel)
+  @response(200, UserWithNumber)
   @response(413, SomeError)
   getUser(
     @param({
       in: 'query',
-      schema: { type: 'array', items: { type: 'number', minItems: 1 } },
+      schema: { type: 'array', items: { type: 'number' } },
       name: 'param',
       allowReserved: true,
       required: false,
+      style: 'form',
+      explode: true,
+      example: '?param=1&param=2&param=3',
     })
-    param: number,
+    param: number[],
     @param.path.number('id') id: number,
     @requestBody({
       required: true,
@@ -51,7 +67,7 @@ export default class GetUserController {
     })
     user: User
   ) {
-    if (Math.random() > 0.5) return new SuccessModel([1, 2, 3, param, id]);
+    return new UserWithNumber({ user: user, numbers: [...param, id] });
 
     return new SomeError('Oops');
   }
